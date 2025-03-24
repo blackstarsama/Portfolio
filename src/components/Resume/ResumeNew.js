f31c9a1a5ef789b571f36cc2ee26bafcb8bcf85f
Fode Sama <magassbangalyfode@gmail.com>
2025-03-24 13:15:34 +0100

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/blaé&ckstarsama/Portfolio/52a84417e686cea06d3e6415c34267986aa94331/sc/Assets/Magassouba%20Fod%C3%A9%20-%20DEV%20WEB.pdff";
+  "https://nbviewer.org/github/blackstarsama/Portfolio/blob/93c33c4500df77d8ed9a89352268e4ce9515b0b9/src/Assets/Fod%C3%A9%20Magassouba%20-%20Mars%202025.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
