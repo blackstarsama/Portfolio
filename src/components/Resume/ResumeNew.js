276a2d4d9ae6035a212f2dfb3c5cd8dc7eee9506
Fode Sama <magassbangalyfode@gmail.com>
2025-03-24 14:05:43 +0100

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,11 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-<<<<<<< Updated upstream
-import pdf from "../../Assets/Magassouba Fodé - DEV WEB.pdf";
-=======
 import pdf from "../../Assets/Fodé Magassouba - Mars 2025.pdf";
->>>>>>> Stashed changes
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -34,7 +30,7 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={pdf}>
+          <Document file={resumeLink}>
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
