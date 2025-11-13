618c859ee2877c47c9e19e2e4535bc15ccea2b39
Fode Sama <magassbangalyfode@gmail.com>
2025-11-13 19:16:45 +0100

    Update_node_version_cv

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/Fodé Magassouba - Mars 2025.pdf";
+import pdf from "../../Assets/Fodé Magassouba 2025.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://nbviewer.org/github/blackstarsama/Portfolio/blob/93c33c4500df77d8ed9a89352268e4ce9515b0b9/src/Assets/Fod%C3%A9%20Magassouba%20-%20Mars%202025.pdf";
+  "https://raw.githubusercontent.com/blackstarsama/Portfolio/main/src/Assets/Fod%C3%A9%20Magassouba%202025.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
