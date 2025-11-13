489ecd0a17dde2dd72837fcf90d074abd6c8c84c
Fode Sama <magassbangalyfode@gmail.com>
2025-11-13 20:26:41 +0100

    Update_projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,6 +4,7 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 
 import cover from "../../Assets/Projects/projet.jpg";
+import cover2 from "../../Assets/Projects/projet2.png";
 
 function Projects() {
   return (
@@ -21,19 +22,21 @@ function Projects() {
             <ProjectCard
               imgPath={cover}
               isBlog={false}
-              title="Project Name"
-              description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-              link="#"
+              title="Sama ecommerce website"
+              description="Une plateforme d'e-commerce moderne et élégante construite avec React. 
+              Découvrez une expérience d'achat fluide avec une interface intuitive et réactive."
+              link="https://github.com/blackstarsama/Sama-Ecommerce"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={cover}
+              imgPath={cover2}
               isBlog={false}
-              title="Project Name"
-              description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-              link="#"
+              title="Admin Dashboard"
+              description="Un tableau de bord moderne, fluide et responsive développé avec React.js, inspiré des interfaces professionnelles d’administration. 
+              Il intègre un mode sombre, des graphiques interactifs, et une gestion dynamique des données (ex : commandes récentes)."
+              link="https://github.com/blackstarsama/admin-dash-app"
             />
           </Col>
 
