4cd96fa09cb5ab82c10d95680aeef94dc8cfd41f
Fode Sama <magassbangalyfode@gmail.com>
2025-11-13 17:52:28 +0100

    Update_dependacies

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -15,49 +15,51 @@ import {
   DiCss3,
 } from "react-icons/di";
 import { SiTensorflow, SiCsharp } from "react-icons/si";
+import ReactTooltip from "react-tooltip";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" data-tip="PHP">
         <DiPhp />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" data-tip="Java">
         <DiJava />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" data-tip="CSS3">
         <DiCss3 />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" data-tip="JavaScript">
         <DiJavascript1 />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" data-tip="Node.js">
         <DiNodejs />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" data-tip="React">
         <DiReact />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" data-tip="Bootstrap">
         <DiBootstrap />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" data-tip="MongoDB">
         <DiMongodb />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" data-tip="MySQL">
         <DiMysql />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" data-tip="Git">
         <DiGit />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" data-tip="C#">
         <SiCsharp />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" data-tip="Python">
         <DiPython />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" data-tip="TensorFlow">
         <SiTensorflow />
       </Col>
+      <ReactTooltip place="top" type="dark" effect="float" />
     </Row>
   );
 }
